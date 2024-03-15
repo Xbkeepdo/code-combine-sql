c916bc05eaa477298dae4c263a270cd1abc93c8e
Xbkeepdo <98145043+Xbkeepdo@users.noreply.github.com>
2024-03-15 17:12:07 +0800

    Add files via upload

---
+++ easyjava-demo/src/main/java/com/easyjava/RunDemoApplication.java
@@ -0,0 +1,10 @@
+package com.easyjava;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class RunDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunDemoApplication.class,args);
+    }
+}
+
+
